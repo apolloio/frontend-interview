f16a9c475888ce7b58b3a26d01fe57e76af2dcde
Kamil Doroszewicz <kamil.doroszewicz@apollo.io>
2025-03-06 08:28:16 +0100

    add new logo, reformat files for consistency

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import apolloLogo from "./assets/apollo-logo.jpeg";
-import styles from "./App.module.css";
-import classnames from "classnames";
+import apolloLogo from './assets/apollo-logo.png';
+import styles from './App.module.css';
+import classnames from 'classnames';
 
 function App() {
   return (
     <div className={styles.root}>
       <a href="https://www.apollo.io/tech-blog" target="_blank">
-        <img
-          src={apolloLogo}
-          className={classnames(styles.logo)}
-          alt="Apollo logo"
-        />
+        <img src={apolloLogo} className={classnames(styles.logo)} alt="Apollo logo" />
       </a>
       <h1>Thanks for interviewing with Apollo!</h1>
       &#8249;Your code here&#8250;
